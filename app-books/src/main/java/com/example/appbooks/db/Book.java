0a7d134805b714312279600e825c5135062545ce
LABORATORIO DE COMPUTADORAS FACULTAD <fing.labcom@uce.edu.ec>
2024-03-01 14:07:41 -0500

    first commit

---
+++ app-books/src/main/java/com/example/appbooks/db/Book.java
@@ -0,0 +1,22 @@
+package com.example.appbooks.db;
+
+import java.math.BigDecimal;
+/*
+@Entity
+@Data
+@Table(name = "books")*/
+public class Book {
+   /* @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+
+    private String isbn;
+    private String title;
+    private BigDecimal price;
+    @Column(name="author_id")
+    private Integer authorId;
+
+*/
+
+}
